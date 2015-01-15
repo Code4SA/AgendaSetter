ae14aff670f0b96efb42b0abe60519be7d9daed3
Jason Norwood-Young <jason@10layer.com>
2015-01-15 18:02:10 +0200

    Config example

---
+++ config_example.js
@@ -0,0 +1,17 @@
+module.exports = {
+	twitter: {
+		consumer_key:         '', 
+		consumer_secret:      '', 
+		access_token:         '', 
+		access_token_secret:  '',
+
+		lists: [
+			{ 
+				slug: "sa-journos-who-tweet", owner_screen_name: "rayjoe" 
+			},
+			{
+				slug: "a-brave-new-world", owner_screen_name: "rayjoe"
+			},
+		],
+	},
+}
